d3ab54772ac7c0e76bc7800577ab72859cd25b6d
jared laser <laserjn@gmail.com>
2012-10-03 09:04:47 -0400

    json feed

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// https://spreadsheets.google.com/feeds/list/0AtGV2K5hN9UZdGhKci1IWlNwS2hObU95YTdKN1Z4OEE/1/public/values?alt=json
+
+jQuery(document).ready(function() {
+
+  $.getJSON('https://spreadsheets.google.com/feeds/list/0AtGV2K5hN9UZdGhKci1IWlNwS2hObU95YTdKN1Z4OEE/1/public/values?alt=json', function(data) {
+  var items = [];
+
+  console.log(data.feed.entry);
+
+});
+
+});
